9b7d7180866276678f09cc868fe207217e44a266
Nilesh Kumar Singh <singhnilesh1998@gmail.com>
2023-04-23 11:47:50 +0530

    BookHub Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
+import { ExploreTopBooks } from "./layouts/HomePage/ExploreTopBooks";
+import { Carousel } from "./layouts/HomePage/Carousel";
+import { Heros } from "./layouts/HomePage/Heros";
+import { LibraryServices } from "./layouts/HomePage/LibraryServices";
+import { Footer } from "./layouts/NavbarAndFooter/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <ExploreTopBooks />
+      <Carousel />
+      <Heros />
+      <LibraryServices />
+      <Footer />
     </div>
   );
 }
---
+++ src/layouts/HomePage/ExploreTopBooks.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const ExploreTopBooks = () => {
+  return (
+    <div className="p-5 mb-4 bg-dark header">
+      <div className="container-fluid py-5 text-white d-flex justify-content-center align-items-center">
+        <div>
+          <h1 className="disply-5 fw-bold ">Find Your Next Adventure</h1>
+          <p className="col-md-8 fs-4">Where would you like to go next?</p>
+          <a
+            href="#"
+            type="button"
+            className="btn main-color btn-lg text-white"
+          >
+            Explore Top Books
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
